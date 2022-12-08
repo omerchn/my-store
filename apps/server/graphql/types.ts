596e96388d277818203b99b664f065cb639da3cf
Omer Cohen <omercohen990@gmail.com>
2022-12-08 12:43:18 +0100

    client, server: added gen dev scripts

--- apps/server/graphql/types.ts
+++ apps/server/graphql/types.ts
@@ -1,4 +1,5 @@
 import { objectType, inputObjectType } from 'nexus'
+import { ObjectDefinitionBlock, InputDefinitionBlock } from 'nexus/dist/core'
 import {
   Item as ItemInterface,
   ItemId as ItemIdInterface,
@@ -6,40 +7,56 @@ import {
   FilterBought as FilterBoughtInterface,
 } from '../__generated__/items-service/items'
 
-function keyIn<I>(key: keyof I) {
-  return key
+function def<I>(
+  t: ObjectDefinitionBlock<any> | InputDefinitionBlock<any>,
+  defObj: Record<
+    keyof I,
+    <FieldName extends string>(name: FieldName, ...params: any) => void
+  >
+): void {
+  for (let key in defObj) {
+    defObj[key].bind(t)(key)
+  }
 }
 
 export const Item = objectType({
   name: 'Item',
   definition(t) {
-    t.nonNull.id(keyIn<ItemInterface>('id'))
-    t.nonNull.string(keyIn<ItemInterface>('name'))
-    t.nonNull.string(keyIn<ItemInterface>('description'))
-    t.nonNull.float(keyIn<ItemInterface>('price'))
-    t.nonNull.boolean(keyIn<ItemInterface>('bought'))
+    def<ItemInterface>(t, {
+      id: t.nonNull.string,
+      name: t.nonNull.string,
+      description: t.nonNull.string,
+      price: t.nonNull.float,
+      bought: t.nonNull.boolean,
+    })
   },
 })
 
 export const ItemId = objectType({
   name: 'ItemId',
   definition(t) {
-    t.nonNull.id(keyIn<ItemIdInterface>('id'))
+    def<ItemIdInterface>(t, {
+      id: t.nonNull.id,
+    })
   },
 })
 
 export const ItemInput = inputObjectType({
   name: 'ItemInput',
   definition(t) {
-    t.nonNull.string(keyIn<ItemInputInterface>('name'))
-    t.nonNull.string(keyIn<ItemInputInterface>('description'))
-    t.nonNull.float(keyIn<ItemInputInterface>('price'))
+    def<ItemInputInterface>(t, {
+      name: t.nonNull.string,
+      description: t.nonNull.string,
+      price: t.nonNull.float,
+    })
   },
 })
 
 export const FilterBought = inputObjectType({
   name: 'FilterBought',
   definition(t) {
-    t.nonNull.boolean(keyIn<FilterBoughtInterface>('bought'))
+    def<FilterBoughtInterface>(t, {
+      bought: t.nonNull.boolean,
+    })
   },
 })
