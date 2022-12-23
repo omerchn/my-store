f9f25f1e30a13830b6de9e9b1e4599bb4787bf4c
omercohen990 <omercohen990@gmail.com>
2022-12-23 14:00:53 +0200

    items-service: added default dev db env variables

--- services/items-service/src/env.ts
+++ services/items-service/src/env.ts
@@ -4,9 +4,16 @@ import path from 'path'
 
 dotenv.config({ path: path.resolve(__dirname, '../.env') })
 
+const devDb = {
+  ASTRA_URI:
+    '54bc0df0-a91e-4903-bb01-62ff347c7469-europe-west1.apps.astra.datastax.com:443',
+  ASTRA_TOKEN:
+    'AstraCS:AZcfSscJdZfWaJzQuCmKEvES:ee83f122464093915868ff82c6ee1b393ea628afca2ba91fa323000215586485',
+}
+
 const envSchema = z.object({
-  ASTRA_URI: z.string(),
-  ASTRA_TOKEN: z.string(),
+  ASTRA_URI: z.string().default(devDb.ASTRA_URI),
+  ASTRA_TOKEN: z.string().default(devDb.ASTRA_TOKEN),
 })
 
 const envParsed = envSchema.safeParse(process.env)
